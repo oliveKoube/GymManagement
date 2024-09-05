15bdbba13e34ea067d20ac15fb4642868ea7c1d5
olive.koube <olive.koube@inexa-ci.com>
2024-09-05 20:03:24 +0000

    please work

--- src/GymManagement.Application/Gyms/Commands/CreateGym/CreateGymCommand.cs
+++ src/GymManagement.Application/Gyms/Commands/CreateGym/CreateGymCommand.cs
@@ -5,5 +5,5 @@ using MediatR;
 
 namespace GymManagement.Application.Gyms.Commands.CreateGym;
 
-[Authorize(Roles = "Admin")]
+//[Authorize(Roles = "Admin")]
 public record CreateGymCommand(string Name, Guid SubscriptionId) : IRequest<ErrorOr<Gym>>;
